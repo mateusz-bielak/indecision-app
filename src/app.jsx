680a96ba28642afd19215c0acfe07b03c93c4630
Mateusz Bielak <mt.bielak@gmail.com>
2017-10-16 20:30:38 +0200

    Initial component structure with event handlers

---
+++ src/app.jsx
@@ -0,0 +1,88 @@
+class IndecisionApp extends React.Component {
+  render() {
+    const title = "Indecision"
+    const subtitle = "Put your life in the hands of a computer"
+    const options = ["Thing one", "Thing two", "Thing three"]
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    )
+  }
+}
+
+class Action extends React.Component {
+  handlePick() {
+    alert("handlePick");
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handlePick}>What should I do?</button>
+      </div>
+    )
+  }
+}
+
+class Options extends React.Component {
+  handleRemoveAll() {
+    alert("handleRemoveAll")
+  }
+  render() {
+    return (
+      <div>
+      <button onClick={this.handleRemoveAll}>Reset all</button>
+        <ol>
+        {
+          [this.props.options.map((option) => <Option key={option} optionText={option}/>)]
+        }
+        </ol>
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <li>{this.props.optionText}</li>
+    )
+  }
+}
+
+class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault()
+    const option = e.target.elements.option.value.trim()
+    if (option) {
+      alert(option)
+    }
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<IndecisionApp />, document.querySelector("#app"))
