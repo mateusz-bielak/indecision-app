a4f15332f6800c03715aad3ca2570d7530f9cf48
Mateusz Bielak <mt.bielak@gmail.com>
2017-10-25 22:10:06 +0200

    Implement gulp tasks with babel compilation & browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,26 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync');
 
-gulp.task('default', function() {});
+gulp.task('babel', () => {
+  gulp.src('src/app.jsx')
+    .pipe(babel({
+      presets: ['env', 'react']
+    }))
+    .pipe(gulp.dest('public/scripts'))
+});
+
+gulp.task('serve', () => {
+  browserSync({
+    server: 'public'
+  });
+  gulp.watch('public/*.html', ['reload']);
+  gulp.watch('public/scripts/*.js', ['reload']);
+  gulp.watch('src/app.jsx', ['babel']);
+});
+
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+gulp.task('default', ['serve']);
