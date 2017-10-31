3b5de8661fd15478a81e97611cc89e8dfb4660a7
Mateusz Bielak <mt.bielak@gmail.com>
2017-10-31 18:22:48 +0100

    Rendering all components

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Action from './Action.js';
+import AddOption from './AddOption.js'
+import Header from './Header.js';
+import Options from './Options.js';
+
+class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.state = {
+      options: []
+    };
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({
+          options
+        }));
+      }
+    } catch (e) {}
+
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    alert(this.state.options[randomNum]);
+  }
+  handleDeleteOptions() {
+    this.setState(() => ({
+      options: []
+    }));
+  }
+  handleDeleteOption(optionToDelete) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToDelete != option)
+    }));
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return "Enter valid value to add item"
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists"
+    }
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat([option])
+    }));
+  }
+  render() {
+    const subtitle = "Put your life in the hands of a computer";
+
+    return (
+      <div>
+        <Header subtitle={ subtitle } />
+        <Action hasOptions={ this.state.options.length > 0 } handlePick={ this.handlePick } />
+        <Options options={ this.state.options } handleDeleteOptions={ this.handleDeleteOptions } handleDeleteOption={ this.handleDeleteOption } />
+        <AddOption handleAddOption={ this.handleAddOption } />
+      </div>
+    )
+  }
+}
+
+export default IndecisionApp;
