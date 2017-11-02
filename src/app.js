1f4eb3724c1eed53272cf3ee17dad8b848ea2ff3
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-02 13:39:07 +0100

    Normalize.css added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp.js';
+import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.querySelector("#app"));
